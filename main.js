5dd620e9b9411f1220a693333c5a202816d31998
robert05hu <robert05_hu@yahoo.com>
2016-08-25 19:59:21 +0200

    repo made
    contact under work

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready( function () {
+
+/*	$("#content").load('content/main.html');
+
+	$('.nav li a').click(function(e){
+
+		e.preventDefault();
+		$("#content").load($(this).attr('href'));
+
+		$('.active').removeClass('active');
+		$(this).parent().addClass('active');
+
+	});
+
+	$('.carousel').carousel({
+		interval: 3000
+	})
+
+*/
+});
